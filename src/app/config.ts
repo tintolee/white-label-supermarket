a5ef0f6268c5279d547f13ed40d5949a828f79d4
tintolee <toso4u@gmail.com>
2025-09-20 13:29:16 +0100

    task: add domain types and discount rule config

---
+++ src/app/config.ts
@@ -0,0 +1,3 @@
+export const BOGOF_ID = 'G95';
+export const DISCOUNT_THRESHOLD = 10;
+export const DISCOUNT_RATE = 0.2;     
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    unit?: string;
+    stock?: number;
+    image?: string;
+};
